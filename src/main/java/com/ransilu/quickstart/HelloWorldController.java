1b3a029ba9713ab4d75675eea8bc04e8a391dd65
RansiluSooriyahetti <ransilusooriyahetti@gmail.com>
2023-11-21 09:30:51 +0530

    commit to HelloWorldController.java

--- src/main/java/com/ransilu/quickstart/HelloWorldController.java
+++ src/main/java/com/ransilu/quickstart/HelloWorldController.java
@@ -1,9 +1,14 @@
 package com.ransilu.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "hello world";
+    }
 
 }
